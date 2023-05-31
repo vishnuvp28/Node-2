6916ac709d9e232cd70e752f2a3eb54c415b5080
vishnuvp28 <priyachandrasekarthik@gmail.com>
2023-05-31 12:48:50 +0530

    upload

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
-import { MongoClient} from "mongodb";
+import { MongoClient, ObjectId} from "mongodb";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 import express from "express"
 
 // const MONGO_URL = "mongodb://127.0.0.1";
-const MONGO_URL = "mongodb+srv://vishnupriya:vishnuvp28@vishnu.7ccelt3.mongodb.net/";
-// const MONGO_URL = process.env.MONGO_URL;
+// const MONGO_URL = "mongodb+srv://vishnupriya:vishnuvp28@vishnu.7ccelt3.mongodb.net/";
+const MONGO_URL = process.env.MONGO_URL;
 const client = new MongoClient(MONGO_URL); // dial
 await client.connect(); // call
 console.log("Mongo is connected !!!  ");
@@ -29,7 +31,7 @@ app.get("/",async function (req, res) {
 // Hall details
 app.get("/hallDetails", async function (req, res) {
   const result = await client
-    .db("b42wd2")
+    .db("guvi")
     .collection("hallData")
     .find({})
     .toArray();
@@ -40,7 +42,7 @@ app.get("/hallDetails", async function (req, res) {
 app.post("/createHall", async function (req, res) {
   const data = req.body;
   const result = await client
-    .db("b42wd2")
+    .db("guvi")
     .collection("hallData")
     .insertMany(data);
   res.send(result);
@@ -50,7 +52,7 @@ app.post("/createHall", async function (req, res) {
 app.get("/hallDetails/:id", async function (req, res) {
   const { id } = req.params;
   const halls = await client
-    .db("b42wd2")
+    .db("guvi")
     .collection("hallData")
     .find({_id: new ObjectId(id)})
     .toArray();
@@ -62,7 +64,7 @@ app.put("/hallBooking/:id", async function (req, res) {
   const { id } = req.params;
   const data = req.body;
   const hall = await client
-    .db("b42wd2")
+    .db("guvi")
     .collection("hallData")
     .findOne({ _id: new ObjectId(id) })
   console.log(hall);
@@ -70,17 +72,17 @@ app.put("/hallBooking/:id", async function (req, res) {
     res.send({ message: "Hall already booked" });
   } else {
     const result = await client
-      .db("b42wd2")
+      .db("guvi")
       .collection("hallData")
       .updateOne({ _id: new ObjectId(id) }, { $set: data });
-    res.send(result);
+      res.send(result);
   }
 });
 
 // List all rooms with booked data
 app.get("/bookedHalls", async function (req, res) {
   const result = await client
-    .db("b42wd2")
+    .db("guvi")
     .collection("hallData")
     .find({ ifBooked: "true" })
     .project({
@@ -94,13 +96,13 @@ app.get("/bookedHalls", async function (req, res) {
       bookingStatus: 1,
     })
     .toArray();
-  res.send(result);
+    res.send(result);
 });
 
 // List all customers with booked data
 app.get("/bookedCustomers", async function (req, res) {
   const result = await client
-    .db("b42wd2")
+    .db("guvi")
     .collection("hallData")
     .find({ ifBooked: "true" })
     .project({
@@ -118,7 +120,7 @@ app.get("/bookedCustomers", async function (req, res) {
 // List how many times a customer has booked the room
 app.get("/noOfTimes", async function (req, res) {
   const result = await client
-    .db("b42wd2")
+    .db("guvi")
     .collection("hallData")
     .aggregate([
       { $group: { _id: "$customerName", count: { $sum: 1 } } },
@@ -127,7 +129,7 @@ app.get("/noOfTimes", async function (req, res) {
     .toArray();
   console.log(result);
   const finalResult = await client
-    .db("b42wd2")
+    .db("guvi")
     .collection("hallData")
     .find({ customerName: result[0]._id })
     .project({
@@ -142,109 +144,6 @@ app.get("/noOfTimes", async function (req, res) {
     .toArray();
   res.send(finalResult);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.get("/hall/details/:id", async (req, res) => {
-//   const { id } = req.params;
-//   // console.log(id);
-//   const specificHall = data.find((hall) => hall.id === id);
-//   res.send(specificHall);
-// });
-
-
-//List all customers with booked data
-
-// app.get("/halldetails/rooms", (req, res) => {
-//   const { ifBooked } = req.query;
-//   console.log(ifBooked);
-//   let filteredHall = data;
-//   if (ifBooked) {
-//     filteredHall = filteredHall.filter((halls) => halls.ifBooked === ifBooked);
-//   }
-// });
-
-
-//List all rooms with booked data
-
-// app.get("/halldetails/customer", (req, res) => {
-//   const { noOfSeats } = req.query;
-//   console.log(noOfSeats);
-//   let noOfSeats = data;
-//   if (noOfSeats) {
-//     filteredHall = filteredHall.filter(
-//       (halls) => halls.noOfSeats >= +noOfSeats
-//     );
-//   }
-// });
-
-
-
-//post-----------------------------------------------
-
-// app.post("/hall/details/", (req, res) => {
-//   const newHall = {
-//     id: data.length + 1,
-//     noOfSeats: req.body.noOfSeats,
-//     amenities: req.body.amenities,
-//     price: req.body.price,
-//     ifBooked: "true",
-//     customerName: req.body.customerName,
-//     date: req.body.date,
-//     startTime: req.body.startTime,
-//     endTrim: req.body.endTrim,
-//     RoomId: req.body.RoomId,
-//     RoomName: req.body.RoomName,
-//   };
-//   console.log(req.body);
-//   data.push(newHall);
-//   res.send(newHall);
-// });
-
-
-app.post("/hall/details/", async function (req, res) {
-  const data = req.body;
-  const result = await client
-    .db("guvi")
-    .collection("hallData")
-    .insertMany(data);
-  res.send(result);
-});
-
-//put--------------------------------------------
-
-// app.put("/hall/details/:id", async (req, res) => {
-//   const { id } = req.params;
-//   const halls = data.find((hall) => hall.id === id);
-
-//   if (halls.ifBooked === "true") {
-//     res.status(400).send("hey hall is already booked");
-//   } else {
-//     halls.date = req.body.date;
-//     halls.startTime = req.body.startTime;
-//     halls.endTime = req.body.endTime;
-//     halls.customerName = req.body.customerName;
-//   }
-
-//   res.status(200).send(data);
-// });
-
-function getCompanyDetails(){
-  return client.db("guvi").collection("company").find().toArray()
-  
-}
 
 
 app.listen(8000, () => console.log("server started"));
